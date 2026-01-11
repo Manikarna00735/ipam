42bf4a358dbee3f58e6aef0ce0810af58e9aaa20
iakash0511 <iakash0511@gmail.com>
2026-01-12 02:44:58 +0530

    completed table structures partially

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,37 +1,5 @@
--- create users table
-CREATE TABLE IF NOT EXISTS users (
-  uuid uuid PRIMARY key default gen_random_uuid(),
-  fullname varchar(255) not null,
-  email varchar(255) NOT NULL UNIQUE,
-  employeeId text,
-  jobTitle text,
-  department text,
-  phone varchar(20),
-  orgIds uuid[],
-  assetColumns jsonb,
-  profileImage text,
-  role text,
-  isActive boolean,
-  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  updatedAt TIMESTAMP WITH TIME ZONE 
-)
-
---create organizations table
-CREATE TABLE IF NOT EXISTS organizations (
-  uuid uuid PRIMARY key default gen_random_uuid(),
-  name text not null,
-  email varchar(255) NOT NULL UNIQUE,
-  industry text,
-  numberOfEmployees integer,
-  address text,
-  phone varchar(20),
-  owneruuid uuid,
-  isActive boolean default true,
-  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  updatedAt TIMESTAMP WITH TIME ZONE 
-)
-
 -- create providers table
+-- mandatory fields: name, slug, orgid, user_id
 CREATE TABLE IF NOT EXISTS providers (
   uuid uuid PRIMARY key default gen_random_uuid(),
   name text not null,
@@ -42,14 +10,16 @@ CREATE TABLE IF NOT EXISTS providers (
   accountcount integer,
   circuitcount integer,
   comments text,
-  org_uuid uuid,
-  isActive boolean default true,
-  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  updatedAt TIMESTAMP WITH TIME zone,
-  user_uuid uuid 
+  orgid varchar(128),
+  isactive boolean default true,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128), 
+  unique(orgid,slug)
 )
 
 -- create regions table
+-- mandatory fields: name, slug, orgid, user_id
 CREATE TABLE IF NOT EXISTS regions (
   uuid uuid PRIMARY key default gen_random_uuid(),
   name text not null,
@@ -59,14 +29,16 @@ CREATE TABLE IF NOT EXISTS regions (
   sitescount integer,
   parent_uuid uuid,
   parent_name text,
-  org_uuid uuid,
-  isActive boolean default true,
-  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  updatedAt TIMESTAMP WITH TIME zone,
-  user_uuid uuid 
+  orgid varchar(128),
+  isactive boolean default true,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128),
+  unique(orgid,slug)
 )
 
 -- create sites table
+-- mandatory fields: name, slug, status, orgid, user_id
 CREATE TABLE IF NOT EXISTS sites (
   uuid uuid PRIMARY key default gen_random_uuid(),
   name text not null,
@@ -85,8 +57,111 @@ CREATE TABLE IF NOT EXISTS sites (
   shippingaddress text,
   orgid varchar(128),
   comments text,
-  status boolean,
-  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  updatedAt TIMESTAMP WITH TIME zone,
-  user_id varchar(128) 
+  status text not null,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128),
+  unique(orgid,slug)
+)
+
+-- create locations table
+-- mandatory fields: name, slug, status, site, orgid, user_id
+CREATE TABLE IF NOT EXISTS locations (
+  uuid uuid PRIMARY key default gen_random_uuid(),
+  name text not null,
+  slug text not null,
+  description text,
+  site text,
+  rackscount int,
+  devicescount int,
+  tagscsv text,
+  tenant text,
+  tenantgroup text,
+  parentid text,
+  docid text,
+  orgid varchar(128),
+  status text not null,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128),
+  unique(orgid,slug)
+)
+
+--create manufacturers table
+-- mandatory fields: name, slug, orgid, user_id
+CREATE TABLE IF NOT EXISTS manufacturers (
+  uuid uuid PRIMARY key default gen_random_uuid(),
+  name text not null,
+  slug text not null,
+  description text,
+  tags text,
+  docid text,
+  orgid varchar(128),
+  isactive boolean default true,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128),
+  unique(orgid,slug)
+);
+
+--create platforms table
+-- mandatory fields: name, slug, orgid, user_id
+CREATE TABLE IF NOT EXISTS platforms (
+  uuid uuid PRIMARY key default gen_random_uuid(),
+  name text not null,
+  slug text not null,
+  description text,
+  tags text,
+  manufacturer text,
+  configtemplate text,
+  napalmdriver text,
+  napalmarguments text,
+  docid text,
+  orgid varchar(128),
+  isactive boolean default true,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128),
+  unique(orgid,slug)
+)
+
+-- create vlans table
+-- mandatory fields: name, status, vid, orgid, user_id
+CREATE TABLE IF NOT EXISTS vlans (
+  uuid uuid PRIMARY key default gen_random_uuid(),
+  name text not null,
+  role text,
+  status text not null,
+  description text,
+  vid text not null,
+  vlangroup text,
+  tag text,
+  tenant text,
+  tenantgroup text,
+  docid text,
+  orgid varchar(128),
+  comments text,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128)
+)
+
+-- create vrfs table
+-- mandatory fields: name, vfsid, orgid, user_id
+CREATE TABLE IF NOT EXISTS vrfs (
+  uuid uuid PRIMARY key default gen_random_uuid(),
+  name text not null,
+  description text,
+  vfsid text not null,
+  tag text,
+  tenant text,
+  tenantgroup text,
+  importtarget text,
+  exporttarget text,
+  docid text,
+  orgid varchar(128),
+  comments text,
+  createdat TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updatedat TIMESTAMP WITH TIME zone,
+  user_id varchar(128)
 )
